bc90e451ad661af38f3eca60767439582a7478c8
kardusimajinasi <12430047+kardusimajinasi@users.noreply.github.com>
2025-10-16 22:16:10 +0700

    penambahan widget pada dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+    // namespace App\Filament\Pages;
+
+    // use Filament\Pages\Dashboard as BaseDashboard;
+    // use Filament\Widgets;
+    // use App\Filament\Widgets\MenuGrid;
+    // use App\Filament\Widgets\AccountWidget;
+
+    // class Dashboard extends BaseDashboard
+    // {
+    //     public function getWidgets(): array
+    //     {
+    //         return [
+    //             \App\Filament\Widgets\AccountWidget::class,
+    //             \App\Filament\Widgets\MenuGrid::class,
+    //         ];
+    //     }
+    // }
+
+
---
+++ resources/views/filament/widgets/menu-grid.blade.php
@@ -0,0 +1,21 @@
+{{-- <x-filament-widgets::widget>
+    <x-filament::section>
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
+            @foreach ([
+                ['label' => 'BALIHO'],
+                ['label' => 'SIARAN KELILING'],
+                ['label' => 'SIARAN RADIO ANAK KONATA'],
+                ['label' => 'TABLOID SOLO BERSERI'],
+                ['label' => 'TABLOID SIBER'],
+                ['label' => 'CETAK MMT'],
+                ['label' => 'RUNNING TEKS'],
+            ] as $item)
+                <div
+                    class="bg-blue text-gray-900 text-center py-10 rounded-xl shadow-sm border
+                    hover:bg-green-200 transition duration-150 ease-in-out cursor-pointer">
+                    <span class="text-lg font-semibold">{{ $item['label'] }}</span>
+                </div>
+            @endforeach
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget> --}}
